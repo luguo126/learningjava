3da5a75eef7d53a1e11b89911c4334a77c14002e
luguo126 <withtz@163.com>
2019-07-26 20:22:27 +0800

    init commit

---
+++ src/main/java/com/github/luguo126/java/App.java
@@ -0,0 +1,13 @@
+package com.github.luguo126.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
